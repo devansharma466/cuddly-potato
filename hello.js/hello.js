1296305fa32605310f09b17840e025f4a9db86d7
Devan Sharma Strathclyde <devansharmastrathclyde@Devans-MacBook-Pro-15.local>
2021-05-14 10:09:02 +0100

    added a function to generate a number between 0 and 1000

--- hello.js/hello.js
+++ hello.js/hello.js
@@ -1,1 +1,9 @@
 console.log('hello')
+
+
+const GenerateRandomNumberBetween0and1000 = () => {
+   let Number = Math.ceil(Math.random()*1001)
+   return Number; 
+}
+
+GenerateRandomNumberBetween0and1000();
